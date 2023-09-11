c82bca64e302638d0207d374930369dcbea23e42
Fadi <mfd4951@rit.edu>
2023-09-11 21:47:26 +0400

    Adding all the files.

---
+++ IntroToPy/tech/ahmed.py
@@ -0,0 +1,4 @@
+color = input("what is your fav color")
+nationality = input("what is yous nationality")
+height = input("what is your height")
+print ("your fav color is" , color, "my nationality is",nationality, "my height is",height)
